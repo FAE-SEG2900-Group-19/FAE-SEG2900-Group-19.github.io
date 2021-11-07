b96ad20f9b659cd59dfc77640b832b7536daaeca
Pradyu Vasudev <pvasu009@uottawa.ca>
2021-11-07 16:40:32 -0500

    form validation?

--- contact.js
+++ contact.js
@@ -1,21 +1,26 @@
 
-var fname = document.getElementById("fname").value;
-var lname = document.getElementById("lname").value;
-var email = document.getElementById("email").value;
-var phonenum = document.getElementById("phonenum").value;
-
-
-
 var fname = document.forms["contactForm"]["fname"].value;
 var lname = document.forms["contactForm"]["lname"].value;
 var email = document.forms["contactForm"]["email"].value;
 var phonenum = document.forms["contactForm"]["phonenum"].value;
-function supportReturn(){
-    if (fname!="" && lname!="" && phonenum!="" && email!=""){
+function supporReturn(){
+    if (fname!=="" && lname!=="" && phonenum!=="" && email!==""){
         alert("We will get back to you soon!")
         return true;
     } else{
         alert("Invalid/empty value(s)")
         return false;
     } 
+}
+
+function supportReturn(contactForm){
+    var inputs = contactForm.getElementsByTagName('input');
+    for (var i=0;i<inputs.length;i++){
+        if (inputs[i].hasAttribute("required")){
+            if(inputs[i].value==""){
+                alert("Please fill out all required fields.")
+            }
+        }
+    }
+    alert("We will get back to you soon!");
 }
