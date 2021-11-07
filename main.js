be5cf66eadd7b86bc6dffc511eea782244e65c76
Pradyu Vasudev <pvasu009@uottawa.ca>
2021-11-07 09:47:45 -0500

    added splash screen intro

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-var a =1;
+let intro = document.querySelector('.intro');
+let animateLogo = document.querySelector('.logo-header');
+let logoSpan=document.querySelectorAll(".animateLogo");
+
+window.addEventListener("DOMContentLoaded",()=>{
+
+    setTimeout(()=>{
+
+        logoSpan.forEach((span,idx)=>{
+            setTimeout(()=>{
+                span.classList.add('active');
+            },(idx+1)*400)
+        });
+
+        setTimeout(()=>{
+            logoSpan.forEach((span,idx)=>{
+
+                setTimeout(()=>{
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                },(idx+1)*50)
+            })
+
+        },2000);
+        setTimeout(()=>{
+            intro.style.top='-100vh'
+        },2300)
+    })
+}) 
